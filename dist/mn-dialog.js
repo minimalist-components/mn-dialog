7a5e126140319a5a5f87a77481659a2f57c87b8c
Darlan Mendonça <darlanmendonca@gmail.com>
2016-09-12 10:15:59 -0300

    first draft

--- dist/mn-dialog.js
+++ dist/mn-dialog.js
@@ -1,2 +1,2 @@
-"use strict";
+"use strict";function showDialog(){dialog.classList.add("opened")}function hideDialog(){dialog.classList.remove("opened")}var trigger=document.querySelector(".show-dialog"),dialog=document.querySelector(".mn-dialog");trigger.addEventListener("click",showDialog),dialog.addEventListener("click",hideDialog);
 //# sourceMappingURL=mn-dialog.js.map
--- sources/scripts/mn-dialog.component.js
+++ sources/scripts/mn-dialog.component.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let trigger = document.querySelector('.show-dialog');
+let dialog = document.querySelector('.mn-dialog');
+
+trigger.addEventListener('click', showDialog);
+dialog.addEventListener('click', hideDialog);
+
+function showDialog() {
+  dialog.classList.add('opened');
+}
+
+function hideDialog() {
+  dialog.classList.remove('opened');
+}
