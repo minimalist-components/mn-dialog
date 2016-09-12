1cac3963d74fa5e249cd65acac1e0f17c1b96d8a
Darlan Mendonça <darlanmendonca@gmail.com>
2016-09-12 10:48:45 -0300

    add motion blur

--- sources/scripts/mn-dialog.component.js
+++ sources/scripts/mn-dialog.component.js
@@ -1,3 +1,4 @@
+/* globals $ */
 'use strict';
 
 let trigger = document.querySelector('.show-dialog');
@@ -5,6 +6,7 @@ let dialog = document.querySelector('.mn-dialog');
 
 trigger.addEventListener('click', showDialog);
 dialog.addEventListener('click', hideDialog);
+setMotionBlur();
 
 function showDialog() {
   dialog.classList.add('opened');
@@ -12,4 +14,34 @@ function showDialog() {
 
 function hideDialog() {
   dialog.classList.remove('opened');
+}
+
+function setMotionBlur() {
+  let blur = document.querySelector('#motion-blur'); // the blur filter
+  let blurFilter = blur.firstElementChild;
+
+  let $element = $('.mn-dialog');
+  let lastPos = $element.offset();
+  let multiplier = 0.25;
+
+  updateMotionBlur();
+
+  function setBlur(x,y){
+    blurFilter.setAttribute('stdDeviation', `${x},${y}`);
+  }
+
+  function updateMotionBlur() {
+    // get the current position of the element
+    let currentPos = $element.offset();
+
+    // calculate the changes from the last frame and apply the multiplier
+    let xDiff = Math.abs(currentPos.left - lastPos.left) * multiplier;
+    let yDiff = Math.abs(currentPos.top - lastPos.top) * multiplier;
+
+    // set the blur
+    setBlur(xDiff,yDiff);
+    lastPos = currentPos;
+
+    requestAnimationFrame(updateMotionBlur);
+  }
 }
